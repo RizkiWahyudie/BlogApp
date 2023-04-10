25cf77b6e6f217e929fdcf96f3118f4d2148d7a4
rizki wahyudie git <rizkiwahyudie@gmail.com>
2023-04-10 09:09:03 +0700

    change client id

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,8 +10,8 @@ export default authHandler;
 const options = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
   adapter: PrismaAdapter(prisma),
