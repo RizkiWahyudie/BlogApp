561e1f152bdba7f33d6a69af713975bf3bfc44d5
rizki wahyudie git <rizkiwahyudie@gmail.com>
2023-04-10 08:26:11 +0700

    solve type

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,10 +6,10 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+  // if (!global.prisma) {
+  //   global.prisma = new PrismaClient();
+  // }
+  prisma = new PrismaClient();
 }
 
 export default prisma;
